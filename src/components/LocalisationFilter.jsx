0a13d5cbe39ee86caa5eb12b95de973a55bf1b28
ba9hh <zedtourheart@gmail.com>
2025-11-03 10:49:23 +0100

    first commit

--- src/components/LocalisationFilter.jsx
+++ src/components/LocalisationFilter.jsx
@@ -7,15 +7,19 @@ const LocalisationFilter = ({ localisation, setLocalisation }) => {
     <>
       {/* Desktop */}
       <div className="sm:flex justify-between py-3 px-8 hidden">
-        <div className="flex items-center bg-white py-2 px-3 rounded-full border-2 gap-2">
+        <div className="flex items-center bg-white py-2 px-3 rounded-2xl border-2 gap-2">
           <img className=" h-4 w-4 text-stone-500" src={search} />
-          <h1 className="text-stone-400 text-sm w-52">Search shops</h1>
+          <input
+            placeholder="Search shops"
+            className="outline-none placeholder:text-stone-400 text-sm w-52"
+          />
+          {/* <h1 className="text-stone-400 text-sm w-52">Search shops</h1> */}
         </div>
         <select
           name="localisation"
           value={localisation}
           onChange={(e) => setLocalisation(e.target.value)}
-          className="h-[36px] text-stone-600 text-[#1c1e21] rounded-[4px] px-[8px] pr-[20px] border border-stone-400 w-40 cursor-pointer"
+          className="h-[36px] text-stone-600 text-[#1c1e21] rounded-[4px] px-[8px] pr-[20px] border border-stone-400 w-40 cursor-pointer outline-none"
         >
           {cities.map((city) => (
             <option key={city} value={city}>
