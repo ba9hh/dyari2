5443569cde725a97e2e0c6e0dd19d2da7cf9b415
ba9hh <zedtourheart@gmail.com>
2026-03-13 17:50:09 +0100

    first commit

--- src/components/DyariLogo.jsx
+++ src/components/DyariLogo.jsx
@@ -40,7 +40,7 @@ const DyariLogo = () => {
       </div>
       <header className="sm:hidden w-full block absolute top-0 z-30 backdrop-blur supports-[backdrop-filter]:bg-white/70 bg-white/60 border-b border-stone-200/60">
         <div className="mx-auto grid max-w-7xl grid-cols-3 items-center gap-4 px-4 py-3 md:grid-cols-[1fr_auto_1fr]">
-          <div className="flex items-center bg-white py-2 px-3 rounded-2xl gap-2 w-fit">
+          <div className="flex items-center bg-white p-1 rounded-2xl gap-2 w-fit">
             <img className=" h-4 w-4 text-stone-500" src={filters} />
           </div>
           <Link to={"/"} className="flex items-center justify-center gap-1">
