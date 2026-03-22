c0032d57bf455b7c2d669a366075d129c570df68
ba9hh <zedtourheart@gmail.com>
2026-03-22 13:53:59 +0100

    first commit

--- src/authentication/ShopForm.jsx
+++ src/authentication/ShopForm.jsx
@@ -32,7 +32,7 @@ const ShopForm = () => {
     // TODO: send data to your API
   };
   return (
-    <>
+    <div>
       <div className="flex justify-between items-center mb-4">
         <Typography variant="h6">Remplir le formulaire:</Typography>
         <img src={dyari} className="w-8" />
@@ -177,7 +177,7 @@ const ShopForm = () => {
           </Button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
