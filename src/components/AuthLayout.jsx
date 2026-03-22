1c4b4ec54071fea3657085ed1fc6bfbb90e6ac86
ba9hh <zedtourheart@gmail.com>
2026-03-22 14:14:36 +0100

    first commit

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
 import loginbg from "@/assets/loginbg.jpg";
-
+import DyariLogo from "./DyariLogo";
 export default function AuthLayout() {
   return (
     <div className="flex justify-center items-center h-screen">
+      <DyariLogo />
       <img
         src={loginbg}
         className="absolute inset-0 h-screen w-full object-cover"
