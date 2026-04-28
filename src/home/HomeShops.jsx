ae92f2e58cb9b8c1832e3440250b177f864ce205
ba9hh <zedtourheart@gmail.com>
2026-04-28 21:12:48 +0100

    first commit

--- src/home/HomeShops.jsx
+++ src/home/HomeShops.jsx
@@ -92,7 +92,7 @@ const HomeShops = () => {
   return (
     <>
       <div
-        className="bg-cover bg-bottom relative bg-white/60"
+        className="bg-cover bg-bottom relative bg-gray-50/60"
         // style={{ backgroundImage: `url(${faza})` }}
       >
         <NavBar />
