060ab534fef638fbefecde649af873fcb3d3740e
ba9hh <zedtourheart@gmail.com>
2026-04-28 12:04:04 +0100

    first commit

--- src/home/HomeShop.jsx
+++ src/home/HomeShop.jsx
@@ -12,6 +12,50 @@ const HomeShop = ({ shop }) => {
       className="bg-white sm:shadow-none py-3 sm:pt-3 sm:pb-3 sm:rounded-md sm:border-b border-b-[4px] border-gray-300 cursor-pointer sm:border"
       onClick={() => openShop()}
     >
+      <div className="relative sm:px-0 ">
+        <div className="flex sm:hidden gap-0 overflow-x-auto scrollbar-hide mt-1">
+          {shop.articles?.slice(0, 3).map((article, index) => (
+            <img
+              key={index}
+              className="flex-shrink-0 w-40 aspect-[12/16] border object-cover"
+              src={article?.article_image ?? biscuit50}
+            />
+          ))}
+        </div>
+
+        {/* Desktop: grid */}
+        <div className="hidden sm:grid sm:grid-cols-3 gap-0 sm:mt-0 border-t border-gray-300 pt-3 px-0.5">
+          {shop.articles?.slice(0, 3).map((article, index) => (
+            <img
+              key={index}
+              className="w-full aspect-square border object-cover"
+              src={article?.article_image ?? biscuit50}
+            />
+          ))}
+        </div>
+        {/* <div className="grid grid-cols-2 sm:grid-cols-3 gap-1 sm:mt-1 mt-1">
+          {shop.articles?.slice(0, 3).map((article, index) => {
+            const hideOnXs = index >= 4 ? "hidden sm:block" : "";
+            return (
+              <img
+                key={index}
+                className={`${hideOnXs} w-full aspect-[12/16] border object-cover`}
+                src={article?.article_image ?? biscuit50}
+                />
+                );
+                })}
+                </div> */}
+        {shop.numberOfArticles > 4 && (
+          <div className="absolute right-0 sm:right-5 bottom-0 bg-gray-100 text-gray-500 font-bold text-md p-2 rounded-ss-md border-2 sm:hidden">
+            {`+${shop.numberOfArticles - 4}`}
+          </div>
+        )}
+        {shop.numberOfArticles > 3 && (
+          <div className="absolute right-0 sm:right-5 bottom-0 bg-gray-100 text-gray-500 font-bold text-md p-2 rounded-ss-md border-2 hidden sm:block">
+            {`+${shop.numberOfArticles - 3}`}
+          </div>
+        )}
+      </div>
       <div className="flex justify-between items-center sm:px-5 px-3 pb-3 sm:pb-3">
         <div className="relative flex items-center flex-1 gap-2">
           <img
@@ -54,50 +98,6 @@ const HomeShop = ({ shop }) => {
           </div>
           <h1 className="text-sm sm:text-base">({shop.total_rating})</h1>
         </div>
-      </div>
-      <div className="relative sm:px-0 ">
-        <div className="flex sm:hidden gap-0 overflow-x-auto scrollbar-hide mt-1">
-          {shop.articles?.slice(0, 3).map((article, index) => (
-            <img
-              key={index}
-              className="flex-shrink-0 w-40 aspect-[12/16] border object-cover"
-              src={article?.article_image ?? biscuit50}
-            />
-          ))}
-        </div>
-
-        {/* Desktop: grid */}
-        <div className="hidden sm:grid sm:grid-cols-3 gap-0 sm:mt-0 border-t border-gray-300 pt-3 px-0.5">
-          {shop.articles?.slice(0, 3).map((article, index) => (
-            <img
-              key={index}
-              className="w-full aspect-square border object-cover"
-              src={article?.article_image ?? biscuit50}
-            />
-          ))}
-        </div>
-        {/* <div className="grid grid-cols-2 sm:grid-cols-3 gap-1 sm:mt-1 mt-1">
-          {shop.articles?.slice(0, 3).map((article, index) => {
-            const hideOnXs = index >= 4 ? "hidden sm:block" : "";
-            return (
-              <img
-                key={index}
-                className={`${hideOnXs} w-full aspect-[12/16] border object-cover`}
-                src={article?.article_image ?? biscuit50}
-              />
-            );
-          })}
-        </div> */}
-        {shop.numberOfArticles > 4 && (
-          <div className="absolute right-0 sm:right-5 bottom-0 bg-gray-100 text-gray-500 font-bold text-md p-2 rounded-ss-md border-2 sm:hidden">
-            {`+${shop.numberOfArticles - 4}`}
-          </div>
-        )}
-        {shop.numberOfArticles > 3 && (
-          <div className="absolute right-0 sm:right-5 bottom-0 bg-gray-100 text-gray-500 font-bold text-md p-2 rounded-ss-md border-2 hidden sm:block">
-            {`+${shop.numberOfArticles - 3}`}
-          </div>
-        )}
       </div>
     </div>
   );
