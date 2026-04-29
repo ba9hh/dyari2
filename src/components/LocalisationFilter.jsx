f836f81a841428013e365470659d4487d3d30309
ba9hh <zedtourheart@gmail.com>
2026-04-29 09:23:53 +0100

    first commit

--- src/components/LocalisationFilter.jsx
+++ src/components/LocalisationFilter.jsx
@@ -18,10 +18,10 @@ const LocalisationFilter = ({ localisation, setLocalisation }) => {
             />
             {/* <h1 className="text-stone-400 text-sm w-52">Search shops</h1> */}
           </div>
-          <div className="flex items-center bg-white py-2 px-3 rounded-[4px] border border-stone-300 gap-2">
+          {/* <div className="flex items-center bg-white py-2 px-3 rounded-[4px] border border-stone-300 gap-2">
             <img className=" h-4 w-4 text-stone-400" src={filters} />
             <h1 className="text-stone-500 text-sm">Filters</h1>
-          </div>
+          </div> */}
         </div>
         <div className="relative w-44">
           <img
