a4ae2f649f2b9cb9440393e591a061be6c6c94db
ba9hh <zedtourheart@gmail.com>
2026-04-29 09:34:23 +0100

    first commit

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,8 +1,8 @@
 const categories = [
-    { name: "Les salés 🍕", link: "salés" },
-    { name: "Les sucrés 🍩", link: "sucrés" },
-    { name: "Un mélange 🍱", link: "" },
-    { name: "Les gâteaux 🎂", link: "gateaux" },
-    { name: "Les biscuits 🍪", link: "biscuit" },
+    { name: "Les salés", link: "salés" },
+    { name: "Les sucrés", link: "sucrés" },
+    { name: "Un mélange", link: "" },
+    { name: "Les gâteaux", link: "gateaux" },
+    { name: "Les biscuits", link: "biscuit" },
 ];
 export default categories;
