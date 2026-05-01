0df77583d4e0044228a50844284b75aacf6b3aa9
ba9hh <zedtourheart@gmail.com>
2026-05-01 12:13:41 +0100

    first commit

--- src/home/NavBar.jsx
+++ src/home/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = () => {
             ) : (
               <div>
                 <Link
-                  className="px-2 pb-1 rounded-lg border-2 text-amber-700 border-amber-500 shadow-lg hidden sm:block"
+                  className="px-2 pb-1 rounded-lg border-2 text-amber-700 border-amber-500 shadow-none hidden sm:block"
                   to={"/login"}
                 >
                   {/* {t("navbar.login")} */}
