e8fcc2a26a7909b599f057802b210c406f503961
ba9hh <zedtourheart@gmail.com>
2026-05-21 11:17:16 +0100

    first commit

--- src/skeleton/shop/SkeletonArticlesShop.jsx
+++ src/skeleton/shop/SkeletonArticlesShop.jsx
@@ -1,11 +1,11 @@
 import Skeleton from "react-loading-skeleton";
 const SkeletonArticlesShop = () => {
   return (
-    <div className="w-full sm:w-2/3 bg-white shadow-md rounded-md py-3">
-      <div className="grid grid-cols-3 gap-x-6 gap-y-6 px-8 mt-4">
-        <Skeleton width={"100%"} height={400} />
-        <Skeleton width={"100%"} height={400} />
-        <Skeleton width={"100%"} height={400} />
+    <div className="w-full sm:w-2/3 bg-white ">
+      <div className="grid grid-cols-3 gap-x-6 gap-y-6 mt-4">
+        <Skeleton className="w-full aspect-square" />
+        <Skeleton className="w-full aspect-square" />
+        <Skeleton className="w-full aspect-square" />
       </div>
     </div>
   );
