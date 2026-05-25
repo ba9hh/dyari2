5d84f25d94c322f0fa9e7c73dd1a964be6d416a2
ba9hh <zedtourheart@gmail.com>
2026-05-25 19:04:38 +0100

    first commit

--- src/home/HomeShops.jsx
+++ src/home/HomeShops.jsx
@@ -67,6 +67,24 @@ const HomeShops = () => {
             setLocalisation={setLocalisation}
           />
         </div>
+        <LocalisationFilter
+          localisation={localisation}
+          setLocalisation={setLocalisation}
+        />
+        <div className="sm:mt-6 sm:mx-2 ">
+          <div className="flex items-center mb-4">
+            <h1 className="sm:text-lg font-bold text-gray-600 ">
+              <span className="underline">Decouvrir les meilleurs</span>
+              <span className="text-amber-600 no-underline">
+                {" "}
+                faits maison{" "}
+              </span>
+              <span className="underline">à Dyari</span> :
+            </h1>
+            <div className="flex-grow border-t border-gray-300 mx-6"></div>
+            <img src={dyari} className="w-7" />
+          </div>
+        </div>
         <ShopsHomeSkeleton />
       </>
     );
