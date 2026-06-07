4e71f09b1617870310e8f832ade765387df52e4c
ba9hh <zedtourheart@gmail.com>
2026-06-07 13:33:52 +0100

    first commit

--- src/shop-profile/shop-articles/ArticlesShop.jsx
+++ src/shop-profile/shop-articles/ArticlesShop.jsx
@@ -43,7 +43,7 @@ const ArticlesShop = ({ shopId }) => {
   if (isError) return <div>Error loading articles</div>;
   return (
     <div className="w-full sm:w-2/3 bg-white sm:bg-gray-100/50 shadow-md rounded-md pt-0 pb-0 sm:border-0 sm:border-t-0">
-      <div className="flex justify-end w-full bg-white">
+      <div className="w-full bg-white h-fit">
         <Button
           fullWidth
           component={Link}
@@ -64,7 +64,7 @@ const ArticlesShop = ({ shopId }) => {
           Add article
         </Button>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 sm:gap-2 sm:px-0 mt-1 sm:bg-gray-100/50">
+      <div className="grid grid-cols-2 md:grid-cols-3 sm:gap-2 sm:px-0 mt-2 sm:bg-gray-100/50">
         {articles?.articles?.map((article, index) => (
           <div key={index} className="">
             <ArticleShop
