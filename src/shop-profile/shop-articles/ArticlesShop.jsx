089194c8de0e48a1a3a88a4675029149be49ccd3
ba9hh <zedtourheart@gmail.com>
2026-06-07 13:47:46 +0100

    first commit

--- src/shop-profile/shop-articles/ArticlesShop.jsx
+++ src/shop-profile/shop-articles/ArticlesShop.jsx
@@ -43,7 +43,7 @@ const ArticlesShop = ({ shopId }) => {
   if (isError) return <div>Error loading articles</div>;
   return (
     <div className="w-full sm:w-2/3 flex flex-col gap-2 bg-white sm:transparent shadow-md rounded-md pt-0 pb-0 sm:border-0 sm:border-t-0">
-      <div className="w-full bg-white">
+      <div className="w-full">
         <Button
           fullWidth
           component={Link}
