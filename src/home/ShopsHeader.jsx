045103b75c09cbbfb40cfcb6bfeb98699ddb43c0
ba9hh <zedtourheart@gmail.com>
2026-06-11 21:13:32 +0100

    first commit

--- src/home/ShopsHeader.jsx
+++ src/home/ShopsHeader.jsx
@@ -6,12 +6,18 @@ const ShopsHeader = () => {
     <div className="flex items-center mb-6">
       <img src={dyari} className="w-7" />
       <div className="flex-grow border-t border-gray-300 mx-6"></div>
-      <h1 className="sm:text-lg font-bold text-gray-600 ">
+      <h1 className="sm:text-lg font-bold text-gray-600 flex items-center gap-1.5">
         <span className="underline">Decouvrir les meilleurs</span>
         <span className="text-amber-600 no-underline"> faits maison </span>
-        <span className="underline">à Dyari</span> :
+        <span className="underline">à Dyari</span>
+        <img
+          src="https://flagcdn.com/tn.svg"
+          width="24"
+          alt="Tunisia"
+          className="ml-0.5"
+        />
       </h1>
-      <div className="flex-grow border-t border-gray-300 mx-6"></div>
+      <span className="flex-grow border-t border-gray-300 mx-6"></span>
       <img src={dyari} className="w-7" />
     </div>
   );
