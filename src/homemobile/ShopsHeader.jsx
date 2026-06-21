7153066f951faf98f2af0d2eda8a0ac42f011c82
ba9hh <zedtourheart@gmail.com>
2026-06-21 16:08:06 +0100

    first commit

--- src/homemobile/ShopsHeader.jsx
+++ src/homemobile/ShopsHeader.jsx
@@ -5,10 +5,9 @@ const ShopsHeader = () => {
   return (
     <div className="flex items-center mb-4 sm:mb-6 px-3 sm:px-0">
       <img src={dyari} className="w-5 sm:w-7 flex-shrink-0" />
-      <div className="flex-grow border-t border-gray-300 mx-3 sm:mx-6"></div>
-      <h1 className="text-sm sm:text-lg font-bold text-gray-600 flex items-center gap-1 text-center whitespace-nowrap">
+      <div className="flex-grow border-t border-gray-300 mx-0 sm:ml-6"></div>
+      {/* <h1 className="text-sm sm:text-lg font-bold text-gray-600 flex items-center gap-1 text-center whitespace-nowrap">
         <span className="underline">Bienvenue </span>
-        {/* <span className="text-amber-600"> faits maison </span> */}
         <span className="underline">à Dyari</span>
         <img
           src="https://flagcdn.com/tn.svg"
@@ -16,8 +15,8 @@ const ShopsHeader = () => {
           alt="Tunisia"
           className="ml-1"
         />
-      </h1>
-      <span className="flex-grow border-t border-gray-300 mx-3 sm:mx-6"></span>
+      </h1> */}
+      <span className="flex-grow border-t border-gray-300 mx-0 sm:mr-6"></span>
       <img src={dyari} className="w-5 sm:w-7 flex-shrink-0" />
     </div>
   );
