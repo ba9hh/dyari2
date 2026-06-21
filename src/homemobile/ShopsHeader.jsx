fcffb0f1f2a5cbc2c80f6613041d91d20b978dbf
ba9hh <zedtourheart@gmail.com>
2026-06-21 16:33:07 +0100

    first commit

--- src/homemobile/ShopsHeader.jsx
+++ src/homemobile/ShopsHeader.jsx
@@ -3,20 +3,19 @@ import dyari from "@/assets/dyari.svg";
 
 const ShopsHeader = () => {
   return (
-    <div className="flex items-center mb-4 sm:mb-6 sm:mt-2 px-3 sm:px-0">
+    <div className="flex items-center mb-4 sm:mb-6 sm:mt-3 px-3 sm:px-0">
       <img src={dyari} className="w-5 sm:w-7 flex-shrink-0" />
-      <div className="flex-grow border-t border-gray-300 mx-0 sm:ml-6"></div>
-      {/* <h1 className="text-sm sm:text-lg font-bold text-gray-600 flex items-center gap-1 text-center whitespace-nowrap">
-        <span className="underline">Bienvenue </span>
-        <span className="underline">à Dyari</span>
+      <div className="flex-grow border-t border-gray-300 mx-0 sm:mx-6"></div>
+      <h1 className="text-sm sm:text-lg font-bold text-gray-600 flex items-center gap-1 text-center whitespace-nowrap">
+        <span className="underline">Bienvenue à Dyari</span>
         <img
           src="https://flagcdn.com/tn.svg"
           width="20"
           alt="Tunisia"
           className="ml-1"
         />
-      </h1> */}
-      <span className="flex-grow border-t border-gray-300 mx-0 sm:mr-6"></span>
+      </h1>
+      <span className="flex-grow border-t border-gray-300 mx-0 sm:mx-6"></span>
       <img src={dyari} className="w-5 sm:w-7 flex-shrink-0" />
     </div>
   );
