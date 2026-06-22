59ad30e17ea3a8734bfb8852d6f0775e8934fd67
ba9hh <zedtourheart@gmail.com>
2026-06-22 20:06:43 +0100

    first commit

--- src/homemobile/ShopsHeader.jsx
+++ src/homemobile/ShopsHeader.jsx
@@ -7,7 +7,9 @@ const ShopsHeader = () => {
       <img src={dyari} className="w-5 sm:w-7 flex-shrink-0" />
       <div className="flex-grow border-t border-gray-300 mx-0 sm:mx-6"></div>
       <h1 className="text-sm sm:text-lg font-bold text-gray-600 flex items-center gap-1 text-center whitespace-nowrap">
-        <span className="underline">Bienvenue à Dyari</span>
+        <span className="">
+          Bienvenue à <span className="underline text-amber-600">Dyari</span>
+        </span>
         <img
           src="https://flagcdn.com/tn.svg"
           width="20"
