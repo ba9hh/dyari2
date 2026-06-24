428e497f1de39943da7eb3868e54feb192e419ca
ba9hh <zedtourheart@gmail.com>
2026-06-24 22:28:11 +0100

    first commit

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -3,13 +3,13 @@ import loginbg from "@/assets/loginbg.jpg";
 import DyariLogo from "./DyariLogo";
 export default function AuthLayout() {
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
       <DyariLogo />
       <img
         src={loginbg}
         className="absolute inset-0 object-cover w-full h-screen"
       />
-      <div className="flex justify-center sm:items-center pt-6 sm:pt-0 h-screen">
+      <div className="flex-1 flex justify-center items-center">
         <Outlet />
       </div>
     </div>
