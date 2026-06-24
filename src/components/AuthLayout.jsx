dbd5a7ed4b30f0dfbb8d8c712b65cbc6c0b44ab0
ba9hh <zedtourheart@gmail.com>
2026-06-24 22:22:19 +0100

    first commit

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -9,7 +9,7 @@ export default function AuthLayout() {
         src={loginbg}
         className="absolute inset-0 object-cover w-full h-screen"
       />
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center sm:items-center pt-6 sm:pt-0 h-screen">
         <Outlet />
       </div>
     </div>
