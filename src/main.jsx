eb04f6a1a22299382ba9befda8dc652809ed586c
ba9hh <zedtourheart@gmail.com>
2026-06-25 20:47:45 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import "./i18n.js";
 import App from "./App.jsx";
 import axios from "axios";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -16,5 +15,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
