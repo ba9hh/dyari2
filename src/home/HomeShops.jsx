1ee20a52a53a56d73c36dd7cd76f07c9aa2824e0
ba9hh <zedtourheart@gmail.com>
2026-06-29 14:17:27 +0100

    first commit

--- src/home/HomeShops.jsx
+++ src/home/HomeShops.jsx
@@ -138,7 +138,7 @@ const HomeShops = () => {
       <div className="sm:mt-0 sm:mx-2 ">
         <ShopsHeader />
         {data?.shops?.length > 0 ? (
-          <div className="lg:grid-cols-2 md:grid-cols-2 grid grid-cols-1 sm:gap-x-6 sm:gap-y-6 sm:border-t">
+          <div className="lg:grid-cols-2 md:grid-cols-2 grid grid-cols-1 sm:gap-x-6 sm:gap-y-6 sm:border-t sm:pt-2">
             {data?.shops?.map((shop) => (
               <HomeShop key={shop.id} shop={shop} />
             ))}
