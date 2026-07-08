369f90ef1e8cc612eb0481b42db09dfbb8a98426
ba9hh <zedtourheart@gmail.com>
2026-07-09 00:49:11 +0100

    first commit

--- src/services/orders/createOrder.jsx
+++ src/services/orders/createOrder.jsx
@@ -8,17 +8,11 @@ export async function createOrder({
   items,
   deliveryType,
 }) {
-  const orderTotalAmount = items.reduce(
-    (sum, itm) => sum + itm.price * itm.quantity,
-    0,
-  );
-
   const { data, error } = await supabase.rpc("create_order_with_items", {
     p_shop_id: shopId,
     p_user_id: userId,
     p_user_phone_number: phoneNumber,
     p_user_needed_date: neededDate,
-    p_order_total_amount: orderTotalAmount,
     p_delivery_type: deliveryType,
     p_items: items.map((item) => ({
       article_id: item.articleId,
